87f19c2f6dbd69ff2799f42950928ccf4b0582b5
po_taka <angel.koilov@gmail.com>
2020-03-07 23:23:30 +0200

    added graphiql

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true],
+    Overblog\GraphiQLBundle\OverblogGraphiQLBundle::class => ['dev' => true],
 ];
