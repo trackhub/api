d836c2431b280cbd03324d0a8ad3358590d41f22
po_taka <angel.koilov@gmail.com>
2020-03-22 00:02:18 +0200

    connected with real repo

--- src/GraphQL/Resolver/ResolverMap.php
+++ src/GraphQL/Resolver/ResolverMap.php
@@ -2,16 +2,21 @@
 
 namespace App\GraphQL\Resolver;
 
-use App\Entity\Track;
-use App\Entity\User\User;
+use App\Repository\TrackRepository;
 use Overblog\GraphQLBundle\Definition\ArgumentInterface;
 
 class ResolverMap extends \Overblog\GraphQLBundle\Resolver\ResolverMap
 {
+    private TrackRepository $trackRepo;
+
+    public function __construct(TrackRepository $trackRepo)
+    {
+        $this->trackRepo = $trackRepo;
+    }
+
     protected function map()
     {
-        // test for DI
-        $prefix = 'test 123';
+        $trackRepo = $this->trackRepo;
 
         return [
             'RootQuery' => [
@@ -19,28 +24,15 @@ class ResolverMap extends \Overblog\GraphQLBundle\Resolver\ResolverMap
                     dump(func_get_args());
                     die;
                 },
-                'tracks' => function ($root, ArgumentInterface $ai, \ArrayObject $resolveInfo) use ($prefix) {
+                'tracks' => function ($root, ArgumentInterface $ai, \ArrayObject $resolveInfo) use ($trackRepo) {
                     if ($root !== null) {
                         throw new \Exception("not implemented");
                     }
 
                     $limit = $ai['limit'];
 
-                    $data = [
-                        (function ($prefix) {
-                            $t = new Track(new User());
-                            $t->setNameEn($prefix . ' Markovo');
-                            return $t;
-                        })($prefix),
-                        (function () {
-                            $t = new Track(new User());
-                            $t->setNameEn('Boikovo');
-                            return $t;
-                        })(),
-                        new Track(new User()),
-                    ];
-
-                    return array_slice($data, 0, $limit);
+                    $tracks = $trackRepo->findAll();
+                    return $tracks;
                 },
             ],
         ];
