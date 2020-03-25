99bb4421f00276c073511521908728d6ed3a5a08
po_taka <angel.koilov@gmail.com>
2020-03-25 22:23:22 +0200

    added api prefix to docker containers

--- docker/web/dev/entrypoint.sh
+++ docker/web/dev/entrypoint.sh
@@ -4,13 +4,6 @@ set -e
 
 usermod -u $WEB_UID www-data
 
-#until nc -z sql 3306
-#do
-#    echo "Waiting for sql..."
-#    sleep 1
-#done
-
-#su www-data bash -c "cd /var/www/script/migration && composer install --no-dev && ./vendor/bin/phinx migrate"
 su www-data bash -c "composer install -d /var/www"
 
 apachectl -D FOREGROUND
--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ set -e
 # In this case assign WEB_UID to 1000
 [[ $UID == 0 ]] && export WEB_UID=1000 || export WEB_UID=$UID
 
-docker build --tag=trackhub-web ./docker/web/
+docker build --tag=trackhub-api-web ./docker/web/
 docker-compose -p track build
 
 if [[ "$1" == "-p" ]]; then
